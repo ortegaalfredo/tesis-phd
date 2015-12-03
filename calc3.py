169bd3f20747200f4aeada5582c94a3d00b78024
Alfred <ortegaalfredo@gmail.com>
2015-12-03 07:31:37 -0300

    Corregidos calculos K y agregado slide con fpgamap

--- calc3.py
+++ calc3.py
@@ -1,16 +1,21 @@
 import math
 
-b1=2.0
-b0=10.0
-n=128.0
-M=4095.0
+b1=4.0
+b0=4.0
+n=8.0
+M=256.0
 
 
-for K in range(1,25):
-	P1=b1/(b1+b0)
-	Pnc1=1
-	P0=b0/(b1+b0)
-	Pnc0=math.pow((1.0-(b1/((b1+b0)*M))),(n*K*b1))
-	print Pnc0
-	Pnc=P1*Pnc1+P0*Pnc0
-	print "K: %d  Pn: %f" % (K,Pnc)
+for K in range(1,30):
+	print K,
+	for b1 in range(2,5):
+		P1=b1/(b1+b0)
+		Pnc1=1
+		P0=b0/(b1+b0)
+		Pnc0=math.pow((1.0-(1.0/M)),(n*K*b1))
+		Pnc0=math.pow(1.0-Pnc0,K)
+		print Pnc0,
+		Pnc=P1*Pnc1+P0*Pnc0
+	print
+
+	#print "K: %d  Pn: %f" % (K,Pnc)
